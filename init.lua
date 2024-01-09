d29fb6db4a1f3cfdf189e22a4b9462a0706847da
xenoxanite <xenoxanite@gmail.com>
2024-01-09 16:52:11 +0600

    starting of pywal color scheme

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 
 require("core.options")
 require("core.keybindings")
+require("core.theme")
 require(".lazy")
 
 -- vim.cmd([[autocmd VimEnter ]])
---
+++ lua/pkgs/pywal/init.lua
@@ -0,0 +1,1 @@
+local colors = require("pkgs.pywal.colors")
